09e1044d3e2bc6b1424efa6d47715cc48a57a8a3
George Gatheca <george.gatheca@galaxy.com>
2025-06-25 17:02:00 +0100

    add override details

--- cometbft/docker-entrypoint.sh
+++ cometbft/docker-entrypoint.sh
@@ -12,13 +12,20 @@ if [[ ! -f /cometbft/.initialized ]]; then
 
   dasel put -f /cometbft/config/config.toml -v "${MONIKER}" moniker
   dasel put -f /cometbft/config/config.toml -v "tcp://0.0.0.0:26658" proxy_app
+
+  dasel put -f /cometbft/config/config.toml -v "tcp://0.0.0.0:${CL_P2P_PORT}" p2p.laddr
+  dasel put -f /cometbft/config/config.toml -v "${COMETBFT_PERSISTENT_PEERS}" p2p.persistent_peers
+
   dasel put -f /cometbft/config/config.toml -v "3s" consensus.timeout_propose
   dasel put -f /cometbft/config/config.toml -v "1s" consensus.timeout_prevote
   dasel put -f /cometbft/config/config.toml -v "1s" consensus.timeout_precommit
   dasel put -f /cometbft/config/config.toml -v "5s" consensus.timeout_commit
+
   dasel put -f /cometbft/config/config.toml -t bool -v false consensus.skip_timeout_commit
+
   dasel put -f /cometbft/config/config.toml -v "nop" mempool.type
   dasel put -f /cometbft/config/config.toml -v "" mempool.wal_dir
+
   dasel put -f /cometbft/config/config.toml -t bool -v true instrumentation.prometheus
 
   touch /cometbft/.initialized
