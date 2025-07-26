b755b475b066dbea4068051dd5f6213ae90d502d
Mario Delgado <mario.delgado@galaxy.com>
2025-07-26 19:21:48 +0100

    always update peers

--- cometbft/docker-entrypoint.sh
+++ cometbft/docker-entrypoint.sh
@@ -14,8 +14,6 @@ if [[ ! -f /cometbft/.initialized ]]; then
   dasel put -f /cometbft/config/config.toml -v "tcp://0.0.0.0:26658" proxy_app
 
   dasel put -f /cometbft/config/config.toml -v "tcp://0.0.0.0:${CL_P2P_PORT}" p2p.laddr
-  dasel put -f /cometbft/config/config.toml -v "${COMETBFT_PERSISTENT_PEERS}" p2p.persistent_peers
-  dasel put -f /cometbft/config/config.toml -v "${COMETBFT_UNCONDITIONAL_PEER_IDS}" p2p.unconditional_peer_ids
 
   dasel put -f /cometbft/config/config.toml -v "3s" consensus.timeout_propose
   dasel put -f /cometbft/config/config.toml -v "1s" consensus.timeout_prevote
@@ -34,6 +32,8 @@ else
   echo "Already initialized!"
 fi
 
+dasel put -f /cometbft/config/config.toml -v "${COMETBFT_PERSISTENT_PEERS}" p2p.persistent_peers
+dasel put -f /cometbft/config/config.toml -v "${COMETBFT_UNCONDITIONAL_PEER_IDS}" p2p.unconditional_peer_ids
 dasel put -f /cometbft/config/config.toml -t int -v 100 p2p.max_num_inbound_peers
 dasel put -f /cometbft/config/config.toml -t int -v 20 p2p.max_num_outbound_peers
 
